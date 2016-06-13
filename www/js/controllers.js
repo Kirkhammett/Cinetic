e3ec75b9eff5edd83e694fc1b8a84f348320f211
Filip Boev <boev.filip@gmail.com>
2016-06-13 14:30:14 +0200

    Adding preliminary search with dummy data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,25 @@
 angular.module('cinetic.controllers', [])
 
-.controller('searchCtrl', function($scope, $q, $http)
+.controller('searchCtrl', ['$scope','$q','$http','$state','$ionicPopup', '$ionicSideMenuDelegate', '$ionicLoading', '$timeout', 
+	function($scope, $q, $http, $state, $ionicPopup, $ionicSideMenuDelegate, $ionicLoading, $timeout)
 {
-	
-})
+
+	$scope.listflag = false;
+	$scope.search = function()
+	{
+		$scope.listflag = true;
+	}
+
+	$scope.clearSearch = function()
+	{
+		$scope.listflag = false;
+		delete $scope.movies;    /// dump search result list
+	}
+
+	$scope.movies = [
+	{title:"Godfather", year: '1970'},
+	{title:"Star Wars", year: '1980'},
+	{title:"Jurassic Park", year: '1990'},
+	{title:"Lord of the Rings", year: '2000'}
+	];
+}]);
