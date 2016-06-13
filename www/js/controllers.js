d9e8929cc6ee660e3441dd943f86d08992828a1b
Filip Boev <boev.filip@gmail.com>
2016-06-13 19:47:58 +0200

    Re-add the dummy data after changes to the view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,24 @@
 angular.module('cinetic.controllers', [])
+.controller('searchCtrl', ['$scope','$q','$http','$state','$ionicPopup', '$ionicSideMenuDelegate', '$ionicLoading', '$timeout', 
+	function($scope, $q, $http, $state, $ionicPopup, $ionicSideMenuDelegate, $ionicLoading, $timeout)
+	{
 
-.controller('searchCtrl', function($scope, $q, $http)
-{
-	
-})
+		$scope.listflag = false;
+		$scope.search = function()
+		{
+			$scope.listflag = true;
+		}
+
+		$scope.clearSearch = function()
+		{
+			$scope.listflag = false;
+		delete $scope.movies;    /// dump search result list
+	}
+
+	$scope.movies = [
+	{title:"Godfather", year: '1970'},
+	{title:"Star Wars", year: '1980'},
+	{title:"Jurassic Park", year: '1990'},
+	{title:"Lord of the Rings", year: '2000'}
+	];
+}]);
