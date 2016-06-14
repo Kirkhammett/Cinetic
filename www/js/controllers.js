639dd34e7b3cf035d216ed09d4b1b90b5239844a
Filip Boev <boev.filip@gmail.com>
2016-06-14 15:24:39 +0200

    Add scroll-top-top feature when displaying movie list

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,36 +1,40 @@
 angular.module('cinetic.controllers', [])
-.controller('searchCtrl', function($scope, $q, $http, $state, $ionicPopup, $ionicSideMenuDelegate, $ionicLoading, $timeout, omdbFactory) 
+.controller('searchCtrl', function($scope, $q, $http, $state, $ionicPopup, $ionicScrollDelegate, $ionicSideMenuDelegate, $ionicLoading, $timeout, omdbFactory) 
 {
-		$scope.SearchData = {};
+	$scope.SearchData = {};
 
-		$scope.getMovies = function(SearchData)
+	$scope.scrollTop = function() {
+		$ionicScrollDelegate.scrollTop(true);
+	};
+
+	$scope.getMovies = function(SearchData)
+	{
+		$ionicLoading.show({
+			template: "Loading data..."
+		})
+		console.log(SearchData.title);
+		var promise = omdbFactory.search(SearchData.title);
+		promise.then(function(results)
 		{
-				$ionicLoading.show({
-				template: "Loading data..."
-			})
-			console.log(SearchData.title);
-			var promise = omdbFactory.search(SearchData.title);
-			promise.then(function(results)
+			$ionicLoading.hide();
+			if(results.Response == "True")
 			{
-				$ionicLoading.hide();
-				if(results.Response == "True")
-				{
-					delete $scope.SearchData.title;
-					$scope.movies = results.Search;
-					console.log($scope.movies);
-				}
-				else
-				{
-					var alertPopup = $ionicPopup.alert({
-						title: 'Error!',
-						template: 'It doesn\'t look like that search worked. Try again or check if you have any misspelled words.'
-					});
-				}
-			});
-		}
+				delete $scope.SearchData.title;
+				$scope.movies = results.Search;
+				console.log($scope.movies);
+			}
+			else
+			{
+				var alertPopup = $ionicPopup.alert({
+					title: 'Error!',
+					template: 'It doesn\'t look like that search worked. Try again or check if you have any misspelled words.'
+				});
+			}
+		});
+	}
 
-		$scope.clearSearch = function()
-		{
+	$scope.clearSearch = function()
+	{
 			delete $scope.movies;    // dump search result list
 		}
 	})
