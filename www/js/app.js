89bb526afed06fcc1f826af4779ca26ffa073c9b
Filip Boev <boev.filip@gmail.com>
2016-06-22 22:47:07 +0200

    Small changes to all titles
    Titles should always be placed in the center no matter what OS they run on. Also fixed issue with images not showing in About page. TODO: Fix the god damn posters not showing when movie lists are generated. Works on Browser, doesn't work on phone.

--- www/js/app.js
+++ www/js/app.js
@@ -2,12 +2,17 @@
 {
   var cinetic = angular.module('cinetic', ['ionic','cinetic.controllers', 'cinetic.routes', 'cinetic.services']);
 
+  cinetic.config(function($ionicConfigProvider) {
+  // change default navbar title to always be centered, even on Android
+  $ionicConfigProvider.navBar.alignTitle('center');
+});
+
   cinetic.run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
       if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
-    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
       // Don't remove this line unless you know what you are doing. It stops the viewport
       // from snapping when text inputs are focused. Ionic handles this internally for
